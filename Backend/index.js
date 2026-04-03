7a74a695bc73136bd5ab2c22b1b1a695daba3d26
rahul-11105 <rahullokhande11105@gmail.com>
2026-04-03 09:54:31 +0530

    index.js updated

--- Backend/index.js
+++ Backend/index.js
@@ -12,13 +12,12 @@ require("dotenv").config();
 
 //middlewares;
 app.use(express.json());
-app.use(cors(
-    {
-        origin: 'http://localhost:3000', 
-        credentials: true,
-    }
-
-));
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true
+  }));
+  app.options("*", cors());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
  
@@ -98,9 +97,9 @@ app.post('/signup', async (req, res) => {
 
         // Setting cookie
         res.cookie('uid', token, {
-            httpOnly: true,
-            secure: false, 
-            sameSite: 'Strict',
+            httpOnly: false,
+            secure: true,
+            sameSite: "None",
         });
 
         console.log("New user registered successfully");
@@ -173,9 +172,9 @@ app.post('/login',async (req,res)=>{
 
                     // Setting cookie
                     res.cookie('uid', token, {
-                        httpOnly: true,
-                        secure: false, 
-                        sameSite: 'Strict',
+                        httpOnly: false,
+                        secure: true,
+                        sameSite: "None",
                     });
 
                     return res.json({
